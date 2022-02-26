8223356d4f6136177fe2505d440816de840d9452
SanthoshPatkar <ssanthoshp159@gmail.com>
2022-02-26 15:53:52 +0530

    user and message model

---
+++ backend/models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose=require('mongoose');
+
+const userSchema=mongoose.Schema({
+     name:{
+         type:String,
+         required:true
+     },
+     email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    dp: {
+        type:String,
+        required:true,
+        default:"https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
+    },
+
+},{
+    timestamps:true
+})
+
+const User=mongoose.model("User",userSchema);
+module.exports=User;

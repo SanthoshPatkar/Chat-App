4d1dc3f3f3f7b937acd229f2e44d4347922fd262
SanthoshPatkar <ssanthoshp159@gmail.com>
2022-03-10 22:07:05 +0530

    side drawer component

--- frontend/src/components/e_components/sideDrawer.jsx
+++ frontend/src/components/e_components/sideDrawer.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { Box, Button, Menu, Text, Tooltip } from '@chakra-ui/react';
+import React, { useState } from 'react'
 
 function SideDrawer() {
+const [search, setSearch] = useState("");
+const [searchResult, setSearchResult] = useState([]);
+const [loading, setLoading] = useState(false)
+const [loadingChat, setLoadingChat] = useState()
+
   return (
-    <div>sideDrawer</div>
+    <>
+    <Box d='flex' justifyContent='space-between' alignItems='center' bg='white' w='100%' p='5px 10px' borderWidth='5px'>
+      <Tooltip label="Search users to chat" hasArrow placement='bottom-end'>
+      <Button variant='ghost' >
+      <i class="fa-solid fa-magnifying-glass"></i>
+      <Text d={{base:"none",md:'flex'}} px='4'>Search User</Text>
+      </Button>
+      </Tooltip>
+      <Text fontSize='2xl' fontFamily='work sans'>
+      Talk Master
+      </Text>
+      <div>
+       <Menu>
+
+       </Menu>
+      </div>
+
+    </Box>
+    </>
   )
 }
 

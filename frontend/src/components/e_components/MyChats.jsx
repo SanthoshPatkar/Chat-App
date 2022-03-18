5b1de1133dde9c91d0c2feb83dd74ff7b5427b98
SanthoshPatkar <ssanthoshp159@gmail.com>
2022-03-18 22:14:24 +0530

    group chat

--- frontend/src/components/e_components/MyChats.jsx
+++ frontend/src/components/e_components/MyChats.jsx
@@ -1,20 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import {ChatState} from '../../context/ChatProvider';
-import axios from 'axios';
-import {Box, Button, Stack, Text, useToast} from '@chakra-ui/react'
-import { AddIcon } from '@chakra-ui/icons';
-import ChatLoading from './ChatLoading';
-import { getSender } from '../../context/ChatExpo';
+import React, { useEffect, useState } from "react";
+import { ChatState } from "../../context/ChatProvider";
+import axios from "axios";
+import { Box, Button, Stack, Text, useToast } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
+import ChatLoading from "./ChatLoading";
+import { getSender } from "../../context/ChatExpo";
+import GroupChatModal from "./GroupChatModal";
 
 const MyChats = () => {
-
   const [loggedUser, setLoggedUser] = useState();
 
-  const { selectedChat,user,setSelectedChat,chats, setChats } = ChatState();
+  const { selectedChat, user, setSelectedChat, chats, setChats } = ChatState();
   const toast = useToast();
 
   const fetchChats = async () => {
-     console.log(user);
+    console.log(user);
     try {
       const config = {
         headers: {
@@ -40,27 +40,32 @@ const MyChats = () => {
   useEffect(() => {
     setLoggedUser(JSON.parse(localStorage.getItem("userInfo")));
     fetchChats();
-  }, []);
+  }, [ ]);
 
   return (
     <>
-    <Box d={{ base: selectedChat ? "none" : "flex", md: "flex" }}
-      flexDir="column"
-      alignItems="center"
-      p={3}
-      bg="white"
-      w={{ base: "100%", md: "31%" }}
-      borderRadius="lg"
-      borderWidth="1px">
-        <Box pb={3}
-        px={3}
-        fontSize={{ base: "28px", md: "30px" }}
-        fontFamily="Work sans"
-        d="flex"
-        w="100%"
-        justifyContent="space-between"
-        alignItems="center">
-          my chat
+      <Box
+        d={{ base: selectedChat ? "none" : "flex", md: "flex" }}
+        flexDir="column"
+        alignItems="center"
+        p={3}
+        bg="white"
+        w={{ base: "100%", md: "31%" }}
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Box
+          pb={3}
+          px={3}
+          fontSize={{ base: "28px", md: "30px" }}
+          fontFamily="Work sans"
+          d="flex"
+          w="100%"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          My Chat
+          <GroupChatModal>
           <Button
             d="flex"
             fontSize={{ base: "17px", md: "10px", lg: "17px" }}
@@ -68,53 +73,54 @@ const MyChats = () => {
           >
             New Group Chat
           </Button>
+          </GroupChatModal>
         </Box>
         <Box
-        d="flex"
-        flexDir="column"
-        p={3}
-        bg="#F8F8F8"
-        w="100%"
-        h="100%"
-        borderRadius="lg"
-        overflowY="hidden"
-      >
-        {chats ? (
-          <Stack overflowY="scroll">
-            {chats.map((chat) => (
-              <Box
-                onClick={() => setSelectedChat(chat)}
-                cursor="pointer"
-                bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
-                color={selectedChat === chat ? "white" : "black"}
-                px={3}
-                py={2}
-                borderRadius="lg"
-                key={chat._id}
-              >
-                <Text>
-                  {!chat.isGroupChat
-                    ? getSender(loggedUser, chat.users)
-                    : chat.chatName}
-                </Text>
-                {chat.latestMessage && (
-                  <Text fontSize="xs">
-                    <b>{chat.latestMessage.sender.name} : </b>
-                    {chat.latestMessage.content.length > 50
-                      ? chat.latestMessage.content.substring(0, 51) + "..."
-                      : chat.latestMessage.content}
+          d="flex"
+          flexDir="column"
+          p={3}
+          bg="#F8F8F8"
+          w="100%"
+          h="100%"
+          borderRadius="lg"
+          overflowY="hidden"
+        >
+          {chats ? (
+            <Stack overflowY="scroll">
+              {chats.map((chat) => (
+                <Box
+                  onClick={() => setSelectedChat(chat)}
+                  cursor="pointer"
+                  bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
+                  color={selectedChat === chat ? "white" : "black"}
+                  px={3}
+                  py={2}
+                  borderRadius="lg"
+                  key={chat._id}
+                >
+                  <Text>
+                    {!chat.isGroupChat
+                      ? getSender(loggedUser, chat.users)
+                      : chat.chatName}
                   </Text>
-                )}
-              </Box>
-            ))}
-          </Stack>
-        ) : (
-          <ChatLoading />
-        )}
+                  {chat.latestMessage && (
+                    <Text fontSize="xs">
+                      <b>{chat.latestMessage.sender.name} : </b>
+                      {chat.latestMessage.content.length > 50
+                        ? chat.latestMessage.content.substring(0, 51) + "..."
+                        : chat.latestMessage.content}
+                    </Text>
+                  )}
+                </Box>
+              ))}
+            </Stack>
+          ) : (
+            <ChatLoading />
+          )}
+        </Box>
       </Box>
-    </Box>
     </>
-  )
-}
+  );
+};
 
 export default MyChats;
